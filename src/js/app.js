3163a5c644cefb4ffa6d66ebe894665945a3dd6c
Rombt <rombt@ukr.net>
2023-03-15 16:18:57 +0200

    что то

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,47 @@
 import { isWebp } from "./modules/isWebp.js";     // проверяет поддерживает ли браузер формат webp
 
-
-
 isWebp();
+
+// burger_menu
+// let burgerMenu = document.querySelector('.menu__burger');
+// let hederMenu = document.querySelector('.heder-menu');
+// burgerMenu.addEventListener("click", () => {
+//    hederMenu.classList.toggle('menu__sm');
+//    burgerMenu.classList.toggle('menu__burger-close');
+// });;
+
+// const swiperDish = new Swiper('.swiper.swiper-dish', {
+//    direction: 'horizontal',
+
+//    breakpoints: {
+//       1250: {
+//          direction: 'vertical',
+//       }
+//    },
+
+//    loop: true,
+//    autoplay: {
+//       delay: 5000,
+//    },
+//    effect: 'fade',
+//    fadeEffect: {
+//       crossFade: true,
+//    },
+//    pagination: {
+//       el: '.swiper-pagination',
+//       clickable: true,
+//       renderBullet: function (index, className) {
+//          return '<span class="' + className + '">  <span class="swiper-pagination__center"> </span> </span>';
+//       },
+//    },
+//    mousewheel: {
+//       invert: true,
+//    },
+// });
+
+
+
+const arrSpoilers = document.querySelectorAll('[data-spoilers]');
+if (arrSpoilers.length > 0) {
+
+}
