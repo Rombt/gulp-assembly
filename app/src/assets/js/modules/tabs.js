626962ecb09dbb16574e1c39a6fe8ed3db0269b6
RomRon <romron@ukr.net>
2024-07-11 18:26:12 +0300

    tabs has been fixed

--- app/src/assets/js/modules/tabs.js
+++ app/src/assets/js/modules/tabs.js
@@ -1,26 +1,20 @@
-const tabs = document.querySelectorAll(".tab");
-
-
-
+const tabs = document.querySelectorAll('.tabs');
 
 if (tabs) {
-    tabs.forEach(tab => {
-        tab.addEventListener("click", e => {
-            const tabName = e.target.dataset.tab;
-            const tabsContainer = e.target.closest(".tabs");
-            const tabsItem = tabsContainer.querySelectorAll(".tabs__item");
+  tabs.forEach(tab => {
+    tab.addEventListener('click', e => {
+      const tabName = e.target.dataset.tab;
 
-            tabsItem.forEach(tabItem => {
-                if (tabItem.getAttribute('data-tab-target') === tabName) {
-                    tabItem.classList.add("active");
-                } else if (tabItem.classList.contains("active")) {
-                    tabItem.classList.remove("active")
-                }
+      const tabsContainer = e.target.closest('.tabs');
+      const tabsItem = tabsContainer.querySelectorAll('.tabs__body');
 
-            })
-
-
-
-        })
-    })
+      tabsItem.forEach(tabItem => {
+        if (tabItem.getAttribute('data-tab-name') === tabName) {
+          tabItem.classList.add('active');
+        } else if (tabItem.classList.contains('active')) {
+          tabItem.classList.remove('active');
+        }
+      });
+    });
+  });
 }
