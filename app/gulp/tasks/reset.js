95d20da94921741361a0d7c04c0e9fdb3acf4e8b
Rombt <rombtnet@gmail.com>
2024-08-21 13:23:17 +0300

    .

--- app/gulp/tasks/reset.js
+++ app/gulp/tasks/reset.js
@@ -1,16 +1,18 @@
 /**
- * 
- * ! Deletes !! ALL !! files from root theme folder and app.path.wpPluginPath  
+ *
+ * ! Deletes !! ALL !! files from root theme folder and app.path.wpPluginPath
  * except app.path.clearWP and app.path.clearHtml
- * 
+ *
  */
 
-export const reset = (done) => {
+export const reset = done => {
+  if (!app.path.clear.src || app.path.clear.src.length === 0) {
+    console.log('ERROR: array clearPath does not exist!!!');
+    return done();
+  }
 
-   if (!app.path.clear.src || app.path.clear.src.length === 0) {
-      console.log("ERROR: array clearPath does not exist!!!");
-      return done();
-   }
+  console.log('app.path.clear.src = ', app.path.clear.src);
+  console.log('app.path.prod = ', app.path.prod);
 
-   return app.plugins.del(app.path.clear.src, { force: true });
-}
+  return app.plugins.del(app.path.clear.src, { force: true });
+};
