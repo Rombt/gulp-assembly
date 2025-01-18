d3e355a1dddbfef548e6e3e3ab8e9e404d00a1e3
Rombt <rombtnet@gmail.com>
2025-01-18 16:14:47 +0200

    tabs.js improved

--- app/src/assets/js/modules/tabs.js
+++ app/src/assets/js/modules/tabs.js
@@ -1,26 +1,29 @@
-const tabs = document.querySelectorAll(".tab");
+const tabsContainers = document.querySelectorAll('.tabs');
 
+if (tabsContainers) {
+  tabsContainers.forEach(tabsContainer => {
+    tabsContainer.addEventListener('click', e => {
+      const tab = e.target.closest('.tabs__title');
+      if (!tab) return; 
 
+      const tabName = tab.dataset.tab;
+      const tabsTitles = tabsContainer.querySelectorAll('.tabs__title');
+      tabsTitles.forEach(tabTitle => {
+        if (tabTitle === tab) {
+          tabTitle.classList.add('tabs__title-active');
+        } else {
+          tabTitle.classList.remove('tabs__title-active');
+        }
+      });
 
-
-if (tabs) {
-    tabs.forEach(tab => {
-        tab.addEventListener("click", e => {
-            const tabName = e.target.dataset.tab;
-            const tabsContainer = e.target.closest(".tabs");
-            const tabsItem = tabsContainer.querySelectorAll(".tabs__item");
-
-            tabsItem.forEach(tabItem => {
-                if (tabItem.getAttribute('data-tab-target') === tabName) {
-                    tabItem.classList.add("active");
-                } else if (tabItem.classList.contains("active")) {
-                    tabItem.classList.remove("active")
-                }
-
-            })
-
-
-
-        })
-    })
+      const tabsItems = tabsContainer.querySelectorAll('.tabs__body');
+      tabsItems.forEach(tabItem => {
+        if (tabItem.getAttribute('data-tab-name') === tabName) {
+          tabItem.classList.add('tabs__body-active');
+        } else {
+          tabItem.classList.remove('tabs__body-active');
+        }
+      });
+    });
+  });
 }
